cae07fe44b37ced5eac6d3d04c34ce171289a673
Aria Williams <awill9579@gmail.com>
2026-02-03 11:07:14 -0600

    Package Spinning Done

--- new-game-project/Package Spinner/spinner.gd
+++ new-game-project/Package Spinner/spinner.gd
@@ -1,6 +1,16 @@
 extends Node3D
 
-@onready var package_center_target: Marker3D = $PackageCenterTarget
+const MOUSE_SENSITIVITY : float = 0.002
 
-func _process(delta: float) -> void:
-	pass
+@export var package : Package
+
+func _input(event: InputEvent) -> void:
+	if event is not InputEventMouseMotion:
+		return
+	if !Input.is_action_pressed("drag"):
+		return
+	
+	drag(event)
+
+func drag(event: InputEventMouseMotion):
+	package.next_rotation_dir = event.relative
--- new-game-project/Package/package.gd
+++ new-game-project/Package/package.gd
@@ -1,8 +1,14 @@
-extends RigidBody3D
+class_name Package extends RigidBody3D
 
 const CENTRAL_FORCE : float = 50.0
+
+var next_rotation_dir : Vector2 = Vector2.ZERO
+var DRAG_TORQUE : float = 10000
 
 @export var target : Marker3D
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	state.apply_central_force((target.global_position - global_position) * CENTRAL_FORCE)
+	state.apply_central_force(Vector3(randf_range(-5, 5), randf_range(-5, 5), 0.0))
+	state.apply_torque(Vector3(next_rotation_dir.y, next_rotation_dir.x, 0.0))
+	next_rotation_dir = lerp(next_rotation_dir, Vector2.ZERO, 0.006)
