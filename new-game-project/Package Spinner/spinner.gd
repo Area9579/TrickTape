5ecae1562f89a1b49c5a66cc8c172b55de5c353c
Aria Williams <awill9579@gmail.com>
2026-02-11 17:11:12 -0600

    Basic coverage value

--- new-game-project/Package Spinner/spinner.gd
+++ new-game-project/Package Spinner/spinner.gd
@@ -1,9 +1,12 @@
 extends Node3D
 
 const MOUSE_SENSITIVITY : float = 0.002
+const COVERAGE_INCREMENT_AMOUNT : float = 0.01
 @onready var package_center_target: Marker3D = $PackageCenterTarget
 
 @export var package : Package
+
+var coverage : float
 
 func _input(event: InputEvent) -> void:
 	if event is not InputEventMouseMotion:
@@ -14,7 +17,10 @@ func _input(event: InputEvent) -> void:
 	drag(event)
 
 func drag(event: InputEventMouseMotion):
+	coverage += COVERAGE_INCREMENT_AMOUNT * abs(event.relative.length())
 	package.next_rotation_dir = event.relative
+	coverage = clamp(coverage, 0, 100)
+	print(coverage)
 
 
 func _on_child_entered_tree(node: Node) -> void:
