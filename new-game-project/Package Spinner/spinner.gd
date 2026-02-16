e710696f6e52954374caffbf8bc35c448e5ff343
Aria Williams <awill9579@gmail.com>
2026-02-16 01:39:35 -0600

    Package Switching is sillly

--- new-game-project/Package Spinner/spinner.gd
+++ new-game-project/Package Spinner/spinner.gd
@@ -2,7 +2,7 @@ class_name Spinner extends Node3D
 ## subdcribe to chopiethecat on youtube c:
 
 const MOUSE_SENSITIVITY : float = 0.002
-const COVERAGE_INCREMENT_AMOUNT : float = 0.1
+const COVERAGE_INCREMENT_AMOUNT : float = 0.08
 
 signal package_finished_wrapping
 
@@ -12,8 +12,10 @@ const MIGUEL = preload("uid://b0daissj7v7do")
 const SKULL = preload("uid://73odhl4c6k8b")
 
 var packages : Array[PackedScene] = [SKULL, BOX, MIGUEL]
+var available_packages = packages.duplicate()
 
 @onready var package_center_target: Marker3D = $PackageCenterTarget
+@onready var spawn_location: Marker3D = $SpawnLocation
 
 @export var package : Package
 @export var tape_dispenser : TapeDispenser
@@ -21,7 +23,7 @@ var packages : Array[PackedScene] = [SKULL, BOX, MIGUEL]
 var coverage : float
 
 func _ready() -> void:
-	packages.shuffle()
+	available_packages.shuffle()
 	go_to_next_package()
 
 func _input(event: InputEvent) -> void:
@@ -33,6 +35,8 @@ func _input(event: InputEvent) -> void:
 	drag(event)
 
 func drag(event: InputEventMouseMotion):
+	if package == null:
+		return
 	var target_input_dir : Vector2 = Vector2(tape_dispenser.global_position.x, tape_dispenser.global_position.y)
 	var spin_effectiveness : float = abs(target_input_dir.normalized().dot(event.relative.normalized()))
 	var spin_power : float = clamp(0, 20,  event.relative.length())
@@ -45,16 +49,26 @@ func drag(event: InputEventMouseMotion):
 		package_finished_wrapping.emit()
 
 func go_to_next_package():
-	if packages.is_empty():
-		return
-	package.queue_free()
-	package = packages.pop_front().instantiate()
+	if available_packages.is_empty():
+		available_packages = packages.duplicate()
+		available_packages.shuffle()
+	delete_old_package(package)
+	package = null
+	await get_tree().create_timer(0.5).timeout
+	package = available_packages.pop_front().instantiate()
 	add_child(package)
+	package.global_position = spawn_location.global_position
 	tape_dispenser.package = package
-	
+
+func delete_old_package(old_package : Package):
+	if old_package == null:
+		return
+	old_package.target = null
+	await get_tree().create_timer(2.0).timeout
+	old_package.queue_free()
 
 func _on_child_entered_tree(node: Node) -> void:
 	if node is not Package:
 		return
-	print("Setting ", node, "'s target to ", package_center_target)
+	#print("Setting ", node, "'s target to ", package_center_target)
 	node.target = package_center_target
--- new-game-project/Tape Dispenser/tape_dispenser_positioner.gd
+++ new-game-project/Tape Dispenser/tape_dispenser_positioner.gd
@@ -8,15 +8,18 @@ extends Node3D
 @onready var dispenser_visuals : Array[Node] = visuals_container.get_children()
 
 @onready var positions : Array[Marker3D] = [left, right, up, down]
-
+@onready var available_positions : Array[Marker3D] = positions.duplicate()
 @export var tape_dispenser : TapeDispenser
 
 func _ready() -> void:
-	positions.shuffle()
+	available_positions.shuffle()
 	set_new_dispenser_location()
 
 func set_new_dispenser_location():
-	var next_position : Marker3D = positions.pop_front()
+	if available_positions.is_empty():
+		available_positions = positions.duplicate()
+		available_positions.shuffle()
+	var next_position : Marker3D = available_positions.pop_front()
 	tape_dispenser.reparent(next_position, false)
 	for sprite in dispenser_visuals:
 		if sprite.name == next_position.name:
